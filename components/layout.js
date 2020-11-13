2ae27017ca376ed567903eecb6077f7710d37a56
Stanley Jeong <jeongstanley@gmail.com>
2020-11-13 11:30:55 -0800

    copyright logo for every case study page and new resume updated

--- components/layout.js
+++ components/layout.js
@@ -53,11 +53,38 @@ export default function Layout({ children, home }) {
         <Link href="/"><a><h1 className={`${"App__logo"} ${"sj"}`}>SJ</h1></a></Link>
         {children}</main>
       {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a><h2>← Back to home</h2></a>
-          </Link>
+        <div>
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a><h2>← Back to home</h2></a>
+            </Link>
+          </div>
+          
+          <footer>
+            <section className="footerLeftSpace"></section>
+            
+            <section className="copyright">
+              <span>
+                HANDCRAFTED
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" 
+                      alt="react logo" 
+                      style={{height: "1.7em", position: "relative", bottom: "-0.47em"}}
+                />
+                © 2020 STAN JEONG
+              </span>
+            </section>
+
+            <section className="socialIcons">
+              <ul>
+                <a href="https://www.linkedin.com/in/jeongstanley/" target="_blank" rel="noopener noreferrer"><li><img src="/social_icons/linkedin.svg" alt="linkedin" style={{height: "1.3em"}}/></li></a>
+                <a href="https://github.com/stanjdev" target="_blank" rel="noopener noreferrer"><li><img src="/social_icons/github.svg" alt="github" style={{height: "1.3em"}}/></li></a>
+                <a href="https://www.instagram.com/stan.wav/" target="_blank" rel="noopener noreferrer"><li><img src="/social_icons/instagram.svg" alt="instagram" style={{height: "1.3em"}}/></li></a>
+              </ul>
+            </section>
+          </footer>
+
         </div>
+        
       )}
     </div>
   )
