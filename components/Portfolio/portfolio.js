a951670899b1c6c8ddd197b201a45fc9dc305562
stanjdev <jeongstanley@gmail.com>
2021-07-21 08:43:39 -0700

    updated resume google drive link

--- components/Portfolio/portfolio.js
+++ components/Portfolio/portfolio.js
@@ -62,7 +62,7 @@ export default function Portfolio() {
           <AnchorLink className={styles.item} href="#top"><li>Home</li></AnchorLink>
           <AnchorLink className={styles.item} href="#projects"><li>Projects</li></AnchorLink>
           <AnchorLink className={styles.item} href="#contact"><li>Contact</li></AnchorLink>
-          <a className={styles.item} href="https://drive.google.com/file/d/1r3zfdiiTK84Wz3n7noAPJSHvlduh6ObN/view?usp=sharing" target="_blank" rel="noopener noreferrer"><li>Resume</li></a>
+          <a className={styles.item} href="https://drive.google.com/file/d/1Y2Jz6266iripiKBbXKpHvZ-tZ1nQDkSD/view?usp=sharing" target="_blank" rel="noopener noreferrer"><li>Resume</li></a>
         </ul>
       </nav>
 
